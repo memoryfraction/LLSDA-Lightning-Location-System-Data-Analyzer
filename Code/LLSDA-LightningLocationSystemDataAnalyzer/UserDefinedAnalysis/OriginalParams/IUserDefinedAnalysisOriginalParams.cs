e41e9cd20e5adad94d1cfd87bf0acc2dee38f233
Rong(Rex) Fan <rong.fan1031@gmail.com>
2019-06-16 21:51:18 -0400

    1.0.6
    新增: UserDefinedAnalysis

---
+++ Code/LLSDA-LightningLocationSystemDataAnalyzer/UserDefinedAnalysis/OriginalParams/IUserDefinedAnalysisOriginalParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LLSDA.Entities
+{
+    public interface IUserDefinedAnalysisOriginalParams
+    {
+        List<UserDefinedShape> UserDefinedShapes
+        {
+            get;
+            set;
+        }
+
+        DateTimePeriod DateTimePeriod { get; set; }
+    }
+}
---
+++ Code/LLSDA-LightningLocationSystemDataAnalyzer/UserDefinedAnalysis/OriginalParams/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace LLSDA.Entities
+{
+    /* 点 */
+    
+    public class Point
+    {
+        public Point() { }
+
+        public Point(double lng, double lat)
+        {
+            this.lng = lng;
+            this.lat = lat;
+        }
+
+        private double lng;
+
+        private double lat;
+        
+        public double Lng
+        {
+            get { return lng; }
+            set { lng = value; }
+        }
+        
+        public double Lat
+        {
+            get { return lat; }
+            set { lat = value; }
+        }
+    }
+}
